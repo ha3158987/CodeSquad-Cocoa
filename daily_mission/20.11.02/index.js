6498f752ca9439c9d50ee3d9b98f427ab4dec7e4
Hannah <ha3158987@gmail.com>
2020-11-03 17:13:08 +0900

    saveExecutionSequence func added

--- daily_mission/20.11.02/index.js
+++ daily_mission/20.11.02/index.js
@@ -1,77 +1,81 @@
-// //[2020.11.02 미션] 다각형의 넓이 구하기
-/*-------------#1. getArea 함수-----------------*/
-//: 다각형의 종류를 변별 후 해당 다각형에 걸맞는 함수를 호출. 주어진 숫자값은 호출 시 인자로 전달.
+//[2020.11.02 미션] 다각형의 넓이 구하기
+/*---------------------------#1. getArea 함수 & 각 도형의 넓이를 구하는 함수-------------------------------*/
 let result;
 let arrOrder = [];
 
+//다각형의 종류를 변별 후 해당 다각형에 걸맞는 함수를 호출. 주어진 숫자값은 호출 시 인자로 전달.
 function getArea(shape, ...givenSize) {
-  console.log("arguments", givenSize);
-  //원
   if (shape === "circle") {
-    return getCircle(...givenSize);
+    result = getCircle(...givenSize);
+    console.log(`원의 넓이/넓이의 합은 ${result}.`);
   }
 
-  //사각형
   else if (shape === "rect") {
-    return getRectangle(...givenSize);
+    result = getRectangle(...givenSize);
+    console.log(`사각형의 넓이는 ${result}.`);
   }
 
-  //사다리꼴
   else if (shape === "trapezoid") {
-    return getTrapezoid(...givenSize);
+    result = getTrapezoid(...givenSize);
+    console.log(`사다리꼴의 넓이는 ${result}.`);
   }
+
+  saveExecutionSequence(shape, result);
 }
 
 //원의 넓이
 function getCircle(...radii) {
-  // arrOrder.push("circle");
-  //원의 갯수 > 1 일 때: 반지름의 값 1~n까지의 모든 원 넓이의 합
   if (radii.length !== 1) {
-    let max = radii[1];
+    const max = radii[1];
     let sum = 0;
     for (var i = 1; i <= max; i++) {
       sum += i * i * Math.PI;
     }
-    arrOrder.push({ circle: sum });
     return sum;
   }
 
-  //원의 갯수 === 1 일 때
-  let square = radii[0] * radii[0];
-  let area = Math.PI * square;
-  arrOrder.push({ circle: area });
+  const square = radii[0] * radii[0];
+  const area = Math.PI * square;
   return area;
 }
-result = getArea("circle", 5, 3);
-console.log(`원의 넓이/넚이의 합은 ${result}.`);
 
 //사각형의 넓이
 function getRectangle(length, width) {
-  let area = length * width;
-  arrOrder.push({ rectangle: area });
+  const area = length * width;
   return area;
 }
-result = getArea("rect", 10, 15);
-console.log(`사각형의 넓이는 ${result}.`);
 
 //사다리꼴의 넓이
 function getTrapezoid(shortBase, longBase, height) {
-  let area = ((shortBase + longBase) / 2) * height;
-  arrOrder.push({ trapezoid: area });
+  const area = ((shortBase + longBase) / 2) * height;
   return area;
 }
-result = getArea("trapezoid", 10, 15, 12);
-console.log(`사다리꼴의 넓이는 ${result}.`);
 
-/*-------------#2 & #3. printExecutionSequence 함수-----------------*/
-//: 호출된 함수의 순서와 각 함수 실행결과를 보여준다.
+
+
+/*-------------#2 & #3. saveExecutionSequence & printExecutionSequence 함수-----------------*/
+//호출된 함수의 순서와 실행결과를 값으로 저장한다.
+function saveExecutionSequence(type, returnedData) {
+  arrOrder.push(`${type} : ${returnedData}`)
+}
+
+//호출된 함수의 순서와 각 함수 실행결과를 보여준다.
 function printExecutionSequence() {
   let num = 0;
   for (var i = 0; i < arrOrder.length; i++) {
     num++;
-    debugger;
-    console.log(`${num}.`, arrOrder[i]);
+    console.log(`${num}. ${JSON.stringify(arrOrder[i])}`);
   }
 }
 
-printExecutionSequence();
+
+
+/*-----------------------------------#test cases-------------------------------------------*/
+function testCases () {
+  getArea("circle", 5, 3);
+  getArea("rect", 10, 15);
+  getArea("trapezoid", 10, 15, 12);
+  printExecutionSequence();
+}
+
+testCases();
