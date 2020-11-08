301824bb940035c5fd08dc3115f7cd60fb55abbf
Hannah <ha3158987@gmail.com>
2020-11-08 15:51:29 +0900

    mission2-2 #4 reinforced

--- daily_mission/20.11.04/array.js
+++ daily_mission/20.11.04/array.js
@@ -1,5 +1,5 @@
 //[2020.11.04 미션2] 배열다루기
-const data = require('./o.js');
+
 /*--------------------------------------------#1. factorial 함수 ----------------------------------------------*/
 /*임의의 숫자(m)를 입력받아 1부터 m까지의 factorial 값을 배열로 담아서 반환하는 함수 만들기*/
 function calculate (num){
@@ -121,42 +121,88 @@ function findAvrg (arr){
 
 /*----------------------------------------------------#4. 배열 만들기 ---------------------------------------------------*/
 /* o.js파일에 주어진 데이터 중에 숫자타입으로만 구성된 요소를 뽑아 배열만들기 */
-// import data from "./o";
-// const data = require('./o.js');
 
-const dataFromO = require("./o");
-const o = dataFromO.data;
+const { o, dataTree } = require("./data");
+let answerArr = [];
 
 function findNumTypeElements (obj) {
-    let answerArr = [];
 
-    Object.keys(obj).forEach(function (key){
-        //예외처리
-        if (typeof obj[key] === "number") {
-            answerArr.push(obj[key]);
-            return;
-        }
-
-        const values = Object.values(obj[key]);
-        const numTypeEls = getNumberTypes(values);
-
-        numTypeEls.forEach(function (el){
-            answerArr.push(el);
-        });
-        // ES6문법으로: answerArr = [...answerArr, ...numTypeEls];
-    })
-
+    const entries = Object.entries(obj);
+    entries.forEach(getNumberTypes);
     return answerArr;
 }
 
 function getNumberTypes (arr) {
-    return arr.filter( el => typeof el === "number");
+
+    for (let key in arr[1]) {
+        if (typeof arr[1][key] === "number") {
+            answerArr.push(key);
+        }
+    }
 }
 
 /*---------------------------------------------------#5. 배열 결과 출력 --------------------------------------------------*/
 /* type이 sk인, name으로 구성된 배열만 출력해본다. */
 // Tip: 재귀로 풀기!!! 재귀로 풀어야 다음 미션을 풀 수 있음.
-const dataTree = data.dataTree;
+// 반복되는 패턴을 찾아야함!!!!!
+// 종료조건(termination case)을 반드시 넣어줘야 한다!!!!!! 재귀 함수 내부에서는 반드시 조건에 따라 재귀를 호출하지 않는 경우가 존재해야 한다. 배열의 길이가 '0'일 때 끝이난다는 걸 알기 때문에 이걸 종료조건으로 줘야한다. 이 조건에 부합하는 경우가 있는지 반드시 확인할 것.
+
+
+//배열명: dataTree
+/*
+- 키 "childnode"의 값으로 있는 배열에 요소가 있는지 확인한다. 요소가 없이(배열이 비어 있을 때까지 재귀)""가 나오면 return.
+- 한 단계 들어갈 때마다 ["type"]의 value를 검사해 "sk" 와 일치하는지 검사한다.
+- obj[type] === "sk" 일 경우, obj["name"] 의 값을 새로운 배열에 담는다.
+*/
+let nextChildNodeArr;
+function findSkTypeName (arr) {
+    //자기자신을 호출해야한다.
+
+     //childnode가 있는 경우
+    nextChildNodeArr = arr.filter(hasChildnode);
+    // console.log("nextChildNodeArr!!!!", nextChildNodeArr);
+    if (nextChildNodeArr.length !== 0) {
+    //arr가 리턴되면(childnode가 있으면) 다시 findsktypename(재귀로 호출)
+        typeCheck(nextChildNodeArr);
+        // findSkTypeName(nextChildNodeArr);
+
+
+        // let arr2 = [];
+        // findSkTypeName(arr2)
+    } else {  //여기서 false를 리턴받으면 재귀 종료
+        return;    //종료조건; childArray가 비어있을 때
+    }
+
+    return arrName;
+
+}
+
+//childnode의 value가 빈 배열이 아닌지 검사; 아닐경우(요소가 있을 경우) 재귀함수 실행
+function hasChildnode (obj){
+    //빈배열일 경우(childnode가 없을 경우)
+    if (obj["childnode"].length === 0) {
+        return [];
+    }
+    // findSkTypeName(obj["shildnode"]);
+    return obj["childnode"]; //true 대신 arr를 반환? 있을 경우 childnode array를 반환하고,
+
+}
+
+
+//type이 "sk"인지 아닌지 확인
+function typeCheck (arr){
+    arr.forEach(function (obj) {
+        if (obj["type"] === "sk") {
+            getObjName (obj);
+        }
+    })
+}
+
+//name의 값을 새 배열에 담기
+const arrName = [];
+function getObjName (obj) {
+    arrName.push(obj["name"]);
+}
 
 /*----------------------------------------------------#6. reduce 만들기.-------------------------------------------------*/
 //Array 의 reduce 메서드처럼 동작하는 myReduce 메서드를 만들자.
@@ -165,8 +211,22 @@ const myReduce = (arr, callback, initialValue) => {
 // const result = myReduce(arr, (next,prev) => {...}, []);
 
 
+/*
+  _.reduce = function (collection, iterator, accumulator) {
+    _.each(collection, function (value) {
+      if (accumulator === undefined) {
+        accumulator = collection[0];
+        return iterator(accumulator, value);
+      }
 
-/*------------------------------------------------------#test cases-----------------------ß------------------------------*/
+      accumulator = iterator(accumulator, value);
+    });
+    return accumulator;
+  };
+*/
+
+
+/*------------------------------------------------------#test cases-----------------------------------------------------*/
 function testCases () {
     console.log("재귀 factorial 함수:", calculate(5));
     console.log("for문 사용한 filter함수:", filterId(peoples));
@@ -174,7 +234,8 @@ function testCases () {
     console.log("각 학생의 평균점수:", findAvrgScoreOfEachStdt(grades));
     console.log("모든 학생의 최고점수의 평균점수:", avrgOfAllStdtBestScore(grades));
     console.log("평균찾기함수 실행:", findAverages(grades));
-    console.log("객체에서 숫자타입 요소로만 이루어진 배열만들기:", findNumTypeElements(o))
+    console.log("객체에서 숫자타입 요소로만 이루어진 키값의 배열만들기:", findNumTypeElements(o));
+    console.log("type이 sk인, name으로 구성된 배열만들기:", findSkTypeName(dataTree));
 }
 
 testCases();
