e9b817d702eb38a5b2007dc16e9a9f8e4205ed2a
Hannah <ha3158987@gmail.com>
2020-11-10 03:13:28 +0900

    mission1 #4 done

--- daily_mission/20.11.09/hashmap.js
+++ daily_mission/20.11.09/hashmap.js
@@ -2,20 +2,20 @@
 
 // - 문자열 키와 문자열 값을 저장하는 해시맵 라이브러리를 구현한다.
 // - 고유한 Hash 함수를 정한다 : name 과 phone number 정보를 저장하는 Contact Hash 함수 만들기!
-//   4. get(String) 해당 키와 매치되는 값을 찾아서 리턴한다.
+
+//   <To-do List>
 //   5. isEmpty() 비어있는 맵인지 Bool 결과를 리턴한다.
 //   6. keys() 전체 키 목록을 [String] 배열로 리턴한다.
 //   7. replace(String key, String value) 키-값으로 기존 값을 대체한다.
 //   8. size() 전체 아이템 개수를 리턴한다.   //하나의 entry를 하나의 아이템을 카운트?
 //   9. clear() 전체 맵을 초기화한다.
-//   10. ES Classes를 활용한 방법으로 구현한다.
 
 //저장소 만들기
 let storageArr = [];
 storageArr.length = 10;
 storageArr = Array.from(storageArr, el => []);
 
-//constactInfo Class 생성자함수 만들기
+//constactInfo Class 생성자함수 만들기 (#10. ES6 Class 활용)
 class contactInfo {
     constructor(name, phoneNumber) {
         this.name = name;
@@ -35,26 +35,31 @@ function convertToIndex (hashCode) {
    return hashCode % storageArr.length;
 }
 
+//innerArray를 반환해주는 함수
+function getInnerArray (key) {
+   const index = convertToIndex(getHashCode(key));
+   return storageArr[index];
+}
+
 /* ----------------- 1. put(String key, String value) 키-값을 추가한다. -------------------*/
 function put (name, phoneNumber) {
 
     const newInfo = new contactInfo(name, phoneNumber);
-    const hashCode = getHashCode(name);
-    const idx = convertToIndex(hashCode);
-    const innerArr = storageArr[idx];
+    const innerArr = getInnerArray(name);
     innerArr.push(newInfo);
+
 }
 
 /* ----------------- 2. remove(String key) 해당 키에 있는 값을 삭제한다. -------------------- */
 //키:밸류 를 한쌍으로 같이 삭제
 function remove (key){
-    const idx = convertToIndex(getHashCode(key));
-    const innerArr = storageArr[idx];
+    const innerArr = getInnerArray(key);
 
     innerArr.forEach(function (elObj){   //innderArr에서 동일한 key를 가진 obj(요소)찾기
 
         for (let keyInside in elObj) {
-            if (keyInside === key) {
+
+            if (elObj[keyInside] === key) {
                 let idx = innerArr.indexOf(elObj);
                 innerArr.splice(idx, 1);
             }
@@ -65,8 +70,7 @@ function remove (key){
 
 /* ----------3. containsKey(String) 해당 키가 존재하는지 판단해서 Bool 결과를 리턴한다.------------ */
 function containsKey (key) {
-    const idx = convertToIndex(getHashCode(key));
-    const innerArr = storageArr[idx];
+    const innerArr = getInnerArray(key);
     const valueArr = Object.values(innerArr);
 
     let hasKey;
@@ -82,9 +86,26 @@ function containsKey (key) {
    return hasKey === true ? true : false;
 }
 
-/* -----------------------------------test case---------------------------------------- */
+/* --------------------4. get(String) 해당 키와 매치되는 값을 찾아서 리턴한다.--------------------- */
+function get (key) {
+    const innerArr = getInnerArray(key);
+
+    let searchResult = `${key}(이)라는 이름을 찾을 수 없습니다.`;
+    if (containsKey(key)) {
+        innerArr.forEach( function (obj) {
+            if (obj["name"] === key )
+                searchResult = obj;
+            }
+        )
+    }
+    return searchResult;
+}
+
+
+/* -----------------------------------test case------------------------------------------ */
 
 function testCase() {
+
 put("Hannah", 1099998888);
 put("Crong", 1011112222);
 put("Beemo", 1033334444);
@@ -93,18 +114,17 @@ put("Goody", 1077778888);
 put("Autumn", 1099991111);
 put("Jun", 1022225555);
 
-console.log("put으로 새로운 contact 추가:", storageArr);
-remove("Hannah");
-console.log("remove로 해당 key(Hannah)에 있는 값 삭제:", storageArr);
+console.log("#1. put으로 새로운 contact 추가:", storageArr);
 
-console.log("해당 key(Crong)가 존재하는 지 확인:", containsKey("Crong"));
-console.log("해당 key(John)가 존재하는 지 확인:", containsKey("John"));
+remove("Hannah");
+console.log("#2. remove로 해당 key(Hannah)에 있는 값 삭제:", storageArr);
+
+console.log("#3. 해당 key(Crong)가 존재하는 지 확인:", containsKey("Crong"));
+console.log("#3. 해당 key(John)가 존재하는 지 확인:", containsKey("John"));
+
+console.log("#4. 해당 key(Beemo)와 매치되는 값을 찾아서 리턴:", get("Beemo"));
+console.log("#4. 해당 key(Gildong)와 매치되는 값을 찾아서 리턴:", get("Gildong"));
 }
 
 testCase();
 
-
-//전화번호 불러올 때
-//var phoneNum = parseInt(obj[name])로 문자열화.
-// phoneNum = '0' + phoneNum.substring(0, 2) + '-'+ phoneNum.substring(2, 6) + '-' + phoneNum.substring(6, 9)
-//"010-9999-8889"
