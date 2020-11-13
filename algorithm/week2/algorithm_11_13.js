43fa7248c79415c7ad498d1a1cde453d453826c2
Hannah <ha3158987@gmail.com>
2020-11-13 18:14:03 +0900

    removed redundant codes

--- algorithm/week2/algorithm_11_13.js
+++ algorithm/week2/algorithm_11_13.js
@@ -3,34 +3,25 @@ function solution(arr, divisor) {
     let answer = [];
 
     arr.forEach(function (a){
-        if (a % divisor === 0) {  //나머지가 없는 경우
-            console.log("a", a);
+        if (a % divisor === 0) {
             answer.push(a);
         }
     })
 
-   answer.sort (function (a, b) {
-       return a - b;
-   })
+   answer.sort((a, b) => a - b);
 
   return answer.length === 0 ? [-1] : answer;
 }
 
 //------------------------------------두 개 뽑아서 더하기-------------------------------------
 function solution(numbers) {
-    var answer = [];
+    let answer = [];
 
     numbers.forEach(function (cur, idx){
-        if (idx === numbers.length - 1){
-            //마지막 인덱스일때는 다음 숫자가 없으므로 그냥 리턴
-            return;
-        }
+        if (idx === numbers.length - 1) return;
 
-        for(let i = idx; i < numbers.length; i++){ //자기 자신부터 그 다음에 오는 숫자들만 loop를 돎.
-            if (numbers[i + 1] === undefined){
-                //마지막 인덱스의 경우 그 다음 숫자가 없기 때문에 리턴
-                return;
-            }
+        for(let i = idx; i < numbers.length; i++){
+            if (numbers[i + 1] === undefined) return;
 
             let sum = cur + numbers[i + 1];
 
@@ -40,11 +31,7 @@ function solution(numbers) {
         }
     })
 
-    answer.sort(function (a, b){
-        return a - b;
-    })
-
-    return answer;
+   return answer.sort((a, b) => a - b);
 }
 
 //----------------------------------------모의고사------------------------------------------
@@ -56,18 +43,15 @@ let dayArr = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
 let numOfDates = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 function solution(a, b) {
-    let answer;
     let sumOfDates = 0;
 
     for (let i = 0; i < a - 1; i++){
-        //그 전달까지 배열에서 인덱스 기준으로 누적하고 해당 달은 b를 더해줌.
         sumOfDates += numOfDates[i];
     }
     sumOfDates += b;
 
     let leftOver = (sumOfDates % 7 === 0) ? 6 : (sumOfDates % 7) - 1;
-    answer = dayArr[leftOver];
-    return answer;
+    return dayArr[leftOver];
 }
 
 //----------------------------------------약수의 합-----------------------------------------
@@ -81,7 +65,7 @@ function solution(n) {
         return 0;
     }
 
-   n % 2 === 0 ? half = n / 2 :  half = (n / 2) + 1;
+    half = ((n % 2) === 0 ? n / 2 : (n / 2) + 1);
 
     for (let i = 2; i <= half; i++) {
         if ((n % i) === 0) {
@@ -89,6 +73,5 @@ function solution(n) {
         }
     }
 
-   answer += (n + 1);
-    return answer;
+    return answer += (n + 1);
 }
