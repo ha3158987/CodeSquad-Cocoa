53384ef9376ed76d6f714fb005bb4214099c1e65
Hannah <ha3158987@gmail.com>
2020-11-15 19:33:21 +0900

    programmers #42840 done

--- algorithm/week2/algorithm_11_13.js
+++ algorithm/week2/algorithm_11_13.js
@@ -35,7 +35,37 @@ function solution(numbers) {
 }
 
 //----------------------------------------모의고사------------------------------------------
+function solution(answers) {
+    const p1 = [1, 2, 3, 4, 5];
+    const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let p1Correct = 0, p2Correct = 0, p3Correct = 0;
 
+    //나머지 연산자: 나누어지는 수(dividend)가 나누는 수(divisor)보다 작을 때 나누어지는 수가 그대로 나머지로 나온다.
+    answers.forEach((el, index) => {
+        if (el === p1[index % p1.length]){
+            p1Correct++;
+        }
+        if (el === p2[index % p2.length]){
+            p2Correct++;
+        }
+        if (el === p3[index % p3.length]){
+            p3Correct++;
+        }
+    });
+
+    //맞힌 문제 갯수가 가장 큰 사람 찾기
+    const answer = [p1Correct, p2Correct, p3Correct];
+    const firstPlace = Math.max(...answer);
+    const winner = [];
+
+    answer.forEach((el, index) => {
+        if (el === firstPlace) {
+            winner.push(index + 1);
+        }
+    });
+    return winner;
+}
 
 //----------------------------------------2016년------------------------------------------
 
