e24dc46589ff3cdc64fe16cdf27cc8373a4f3c1e
Hannah <ha3158987@gmail.com>
2020-11-28 21:33:12 +0900

    fix: fixed bug

--- algorithm/week4/reverse_integer.js
+++ algorithm/week4/reverse_integer.js
@@ -4,9 +4,7 @@
  */
 var reverse = function(x) {
     let xArr = JSON.stringify(x).split("");//["-", 2", "3", "4"]
-
     let posArr = xArr.filter(x => isNaN(x * 1) === false);//["2", "3", "4"]
-
     let reversed = posArr.reverse(); //["4", "3", "2"]
     let answer = reversed.join(""); //"432"
 
