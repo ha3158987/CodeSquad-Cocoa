eef8566400d3460a0084544cce990ff05f78465d
Hannah <ha3158987@gmail.com>
2020-12-03 18:16:51 +0900

    feat: enable loading saved info from local storage

--- final_project/app.js
+++ final_project/app.js
@@ -11,7 +11,7 @@ UI에 렌더링 하는 작업을 수행한다.
 3. 추가되는 각각의 이미지에 <a>링크를 걸어 해당 Product 인스턴스의 정보를 보여줄 수 있도록 한다.
 혹은 마우스 hover시 말풍선이 나오는 구조도 고려.(https://nanati.me/css-balloons-menu/)
 */
-const items = []; //추가된 모든 아이템의 정보를 객체 단위로 갖는 배열.
+let items = []; //추가된 모든 아이템의 정보를 객체 단위로 갖는 배열.
 let index = 0;
 
 /**************************** Product 클래스: input정보를 가져오고 제품이 등록될 때마다 인스턴스를 생성 ***************************/
@@ -43,6 +43,7 @@ class Product {
     newObj.date = inputArray[2];
     newObj.rating = inputArray[3];
     newObj.review = inputArray[4];
+    newObj.image = inputArray[5];
     newObj.index = index; //현재 추가된 제품사진의 인덱스를 저장.
     index++;
     console.dir(newObj);
@@ -67,6 +68,7 @@ class Model {
       items.push(newItemObj);
       this.saveToLocalStorage();
       this.resetAllForm();
+      //deleteAllErrorMsg
       console.dir(localStorage); //확인용
     }
     return inputArray;
@@ -103,6 +105,25 @@ class Model {
 /**************************************** View 클래스: UI 관련 작업들을 관리 *******************************************/
 class View {
 
+  showSavedItems(savedItems) { //local storage에서 가져온 items배열들의 요소(item)들을 UI drawer에서 보여주기.
+    console.log(savedItems);
+    const container = document.querySelector(".img-grid");
+    let innerContainer = ``;
+    let count = 0;
+
+    savedItems.forEach(item => {
+      innerContainer += this.makeImageContainingLiTag(item, count);
+      console.log(innerContainer);
+      count++;
+    })
+    container.innerHTML = innerContainer;
+  }
+
+  makeImageContainingLiTag(item, count){
+    const liContainer = `<li class="inner-img-container"><img class="item-image${count}" src="${item["image"]}"></li>`;
+    return liContainer;
+  }
+
   showPreviewImage() {
     let reader = new FileReader();
     let imgFile;
@@ -114,10 +135,6 @@ class View {
       reader.readAsDataURL(imgFile); //이미지 파일 읽어올 때
     });
   }
-
-    //   removePreviewImage() {
-
-    //   }
 
   showErrorMsg(inputArray) {
     const inputName = [
@@ -170,6 +187,9 @@ class View {
     //문제1:이미지는 인풋값을 새로 넣어줘도 에러메세지가 사라지지 않음. preview 이미지도 없애야함.
     //문제2:한꺼번에 여러항목을 수정하면 에러메세지가 한번에 사라지지 않음.
   }
+
+  //   removePreviewImage() {
+  //   }
 }
 /*************************************** Controller 클래스: Model과 View의 중계역할 ***********************************/
 class Controller {
@@ -181,12 +201,18 @@ class Controller {
 
   init() {
     this.addEvent();
+    this.loadLocalStorage();
+  }
+
+  loadLocalStorage(){
+    if(localStorage.getItem('items') !== null) {
+      items = this.model.loadFromLocalStorage();
+      this.view.showSavedItems(items);
+    }
   }
 
   addEvent() {
     const addButton = document.querySelector('.add-button');
-    const parsedItems = this.model.loadFromLocalStorage(); // 저장된 아이템을 UI로 보여주기
-    console.log(parsedItems); //확인용
     this.view.showPreviewImage();
     addButton.addEventListener('click', this.eventCallback.bind(this));
   }
